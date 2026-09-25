024f7d524148c44efa5590916176668a82170ea5
ellie-blip <ilhamdaddihammou@gmail.com>
2026-09-25 12:40:12 +0100

    fix CORS for frontend deployement

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(
   cors({
     origin: [
       "https://habittracker-six-rho.vercel.app",
+      "https://elliehabittracker-lpyv9ft8a-bytequestlog-spec.vercel.app",
       "http://localhost:5173",
     ],
   }),
